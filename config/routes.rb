05cea07d1f6a6d83b816cb14f71fa7e9e897a42a
sho037 <103867061+sho037@users.noreply.github.com>
2022-08-10 13:53:39 +0900

    新規ページ posts/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/new'
+  # get 'posts/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
+  # post 'posts' => 'posts#create'
+  resources :posts
 end

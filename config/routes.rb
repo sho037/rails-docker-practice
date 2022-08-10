fc3d14d0ed5f0038fe4b1c50d9b48cb7fbf6ecd3
sho037 <103867061+sho037@users.noreply.github.com>
2022-08-10 13:24:36 +0900

    新しいコントローラーとビューを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'posts/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
 end

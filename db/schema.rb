7f7e12795b6cae2dd2c441216d14d9c98c69c709
sho037 <103867061+sho037@users.noreply.github.com>
2022-08-11 16:52:40 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_08_123914) do
+ActiveRecord::Schema.define(version: 2022_08_11_074601) do
 
   create_table "posts", charset: "utf8mb4", force: :cascade do |t|
     t.string "location"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_08_08_123914) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
+    t.string "image_id"
   end
 
 end
